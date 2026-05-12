1a0431b66b007adafc7fd2f64f214335173adb0f
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-05-12 17:38:00 +0700

    code komposisi

---
+++ komposisi.cpp
@@ -0,0 +1,21 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+using namespace std;
+
+class manusia
+{
+public:
+    string name;
+    jantung varjantung;
+
+    manusia(string pname)
+        : name(pname)
+    {
+        cout << name << "hidup\n";
+    }
+    ~manusia()
+    {
+        cout << name << "mati\n";
+    }
+};
+#endif
