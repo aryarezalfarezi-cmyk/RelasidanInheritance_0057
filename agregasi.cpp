d718a54c7056fa9e975bfacdc334da2c651eff70
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-05-12 17:15:44 +0700

    end agregasi

--- agregasi.cpp
+++ agregasi.cpp
@@ -7,3 +7,26 @@ int main()
 {
     Ibu *varIbu = new Ibu("Dini");
     Ibu *varIbu2 = new Ibu("Novi");
+    anak *varAnak1 = new anak("Tono");
+    anak *varAnak2 = new anak("Rini");
+    anak *varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    system("pause");
+    return 0;
+}
