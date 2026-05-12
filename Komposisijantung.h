a624cd5a5b9458c67580618abb344ab018181590
aryarezalfarezi-cmyk <aryarezalfarezi@gmail.com>
2026-05-12 17:27:28 +0700

    end code jantung

--- Komposisijantung.h
+++ Komposisijantung.h
@@ -2,3 +2,18 @@
 #define JANTUNG_H
 
 #include <iostream>
+class jantung
+{
+public:
+    jantung()
+    {
+        std::cout << "jantung dihidupkan\n";
+    }
+
+    ~jantung()
+    {
+        std::cout << "jantung dimatikan\n";
+    }
+};
+
+#endif
